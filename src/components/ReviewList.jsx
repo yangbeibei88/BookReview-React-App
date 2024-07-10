8f929a469ec43dde4c228760d8cf1c034f3ff828
BeibeiYang <chrisyangber@hotmail.com>
2024-07-10 14:29:48 +1000

    added fade animation with framer motion

--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from "framer-motion";
 import { ReviewItem } from "./ReviewItem.jsx";
 export const ReviewList = ({ reviews, handleDelete }) => {
   if (reviews.length === 0) {
@@ -5,12 +6,34 @@ export const ReviewList = ({ reviews, handleDelete }) => {
   } else {
     return (
       <div className="review-list">
-        {reviews.map((item) => {
-          return (
-            <ReviewItem key={item.id} item={item} handleDelete={handleDelete} />
-          );
-        })}
+        <AnimatePresence>
+          {reviews.map((item) => {
+            return (
+              <motion.div
+                key={item.id}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <ReviewItem
+                  key={item.id}
+                  item={item}
+                  handleDelete={handleDelete}
+                />
+              </motion.div>
+            );
+          })}
+        </AnimatePresence>
       </div>
     );
+    // return (
+    //   <div className="review-list">
+    //     {reviews.map((item) => {
+    //       return (
+    //         <ReviewItem key={item.id} item={item} handleDelete={handleDelete} />
+    //       );
+    //     })}
+    //   </div>
+    // );
   }
 };
