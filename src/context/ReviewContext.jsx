d3455577c4c28ac3e8cb0092992074e7a0bb4c44
BeibeiYang <chrisyangber@hotmail.com>
2024-07-14 18:28:41 +1000

    add data to json database

--- src/context/ReviewContext.jsx
+++ src/context/ReviewContext.jsx
@@ -1,9 +1,9 @@
 import { createContext, useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 export const ReviewContext = createContext();
 
 export const ReviewProvider = ({ children }) => {
+  const server = "http://localhost:8080";
   const [reviews, setReviews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [reviewEdit, setReviewEdit] = useState({
@@ -17,19 +17,25 @@ export const ReviewProvider = ({ children }) => {
 
   // fetch data
   const fetchData = async () => {
-    const response = await fetch(
-      `http://localhost:8080/reviews?_sort=id&_order=desc`,
-    );
+    const response = await fetch(`${server}/reviews?_sort=id&_order=desc`);
     const data = await response.json();
     // console.log(data);
     setReviews(data);
     setIsLoading(false);
   };
 
-  const addReview = (newReview) => {
-    newReview.id = uuidv4();
+  const addReview = async (newReview) => {
+    const response = await fetch(`${server}/reviews`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newReview),
+    });
+
+    const newData = await response.json();
     console.log(newReview);
-    setReviews([newReview, ...reviews]);
+    setReviews([newData, ...reviews]);
   };
 
   const deleteReview = (id) => {
