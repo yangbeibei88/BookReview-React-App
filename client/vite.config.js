722d7bab49f7bc90a6a99834499d7e56efe0da6a
BeibeiYang <chrisyangber@hotmail.com>
2024-07-15 13:45:01 +1000

    added base

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/BookReview-React-App/",
   root: "client/src",
   build: {
     outDir: "../dist",
