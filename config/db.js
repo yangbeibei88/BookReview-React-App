fc4aa19e287ad414b1db0b696f4c912937f1fd07
BeibeiYang <chrisyangber@hotmail.com>
2024-07-18 07:05:13 +1000

    catch error from db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 };
