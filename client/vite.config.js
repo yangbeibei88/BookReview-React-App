a228056efa92a240ee832ea3610fd89a12a8fdc1
BeibeiYang <chrisyangber@hotmail.com>
2024-07-18 08:36:36 +1000

    regenerate dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/BookReview-React-App/",
   root: "client/src",
   build: {
     outDir: "../dist",
